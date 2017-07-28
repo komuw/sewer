dab0d8da203a2dc08e4bc657346b8d6eb06b2fae
komu <komuw05@gmail.com>
2017-07-29 02:28:59 +0300

    add mock as a requirement for tests

--- setup.py
+++ setup.py
@@ -79,7 +79,8 @@ setup(
     # for example:
     # $ pip install -e .[dev,test]
     extras_require={
-        'dev': ['coverage', 'pypandoc', 'twine', 'wheel', 'yapf', 'flake8'],
+        'dev':
+        ['coverage', 'pypandoc', 'twine', 'wheel', 'yapf', 'flake8', 'mock'],
     },
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
