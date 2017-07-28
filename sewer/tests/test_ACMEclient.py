c2fc899cfdfbd428766e8af4965cc1a2b9f4f274
komu <komuw05@gmail.com>
2017-07-29 02:29:33 +0300

    add the first test

--- sewer/tests/test_ACMEclient.py
+++ sewer/tests/test_ACMEclient.py
@@ -2,31 +2,46 @@
 # not to pollute the global namespace.
 # see: https://python-packaging.readthedocs.io/en/latest/testing.html
 
-# from unittest import TestCase
+import mock
+from unittest import TestCase
 
-# import funniest
+import sewer
 
-# class TestJoke(TestCase):
-#     def test_is_string(self):
-#         s = funniest.joke()
-#         self.assertTrue(isinstance(s, basestring))
+from . import test_utils
 
-# The best way to get these tests going (particularly if you're not sure what to use) is Nose.
-# With those files added, it's just a matter of running this from the root of the repository:
 
-# $ pip install nose
-# $ nosetests
-# To integrate this with our setup.py, and ensure that Nose is installed when we run the tests, we'll add a few lines to setup():
+class TestACMEclient(TestCase):
 
-# setup(
-#     ...
-#     test_suite='nose.collector',
-#     tests_require=['nose'],
-# )
-# Then, to run tests, we can simply do:
+    def setUp(self):
+        self.domain_name = 'example.com'
+        with mock.patch('requests.post') as mock_requests_post, mock.patch(
+                'requests.get') as mock_requests_get:
+            mock_requests_post.return_value = test_utils.MockResponse()
+            mock_requests_get.return_value = test_utils.MockResponse()
 
-# $ python setup.py test
-# Setuptools will take care of installing nose and running the test suite.
+            self.dns_class = test_utils.ExmpleDnsProvider()
+            self.client = sewer.Client(
+                domain_name=self.domain_name, dns_class=self.dns_class)
+
+    def tearDown(self):
+        pass
+
+    def test_user_agent_is_generated(self):
+        with mock.patch('requests.post') as mock_requests_post, mock.patch(
+                'requests.get') as mock_requests_get:
+            content = """
+                          {"challenges": [{"type": "dns-01", "token": "example-token", "uri": "example-uri"}]}
+                      """
+            mock_requests_post.return_value = test_utils.MockResponse(
+                content=content)
+            mock_requests_get.return_value = test_utils.MockResponse(
+                content=content)
+
+            for i in [
+                    'python-requests', 'sewer', 'https://github.com/komuW/sewer'
+            ]:
+                self.assertIn(i, self.client.User_Agent)
+
 
 # from unittest import TestCase
 # from funniest.command_line import main
