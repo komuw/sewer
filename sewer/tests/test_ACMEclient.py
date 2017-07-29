1ef9ed050af55662dcb3816d9115cfefb0cbeb04
komu <komuw05@gmail.com>
2017-07-29 03:33:45 +0300

    test notify_acme_challenge_set is called

--- sewer/tests/test_ACMEclient.py
+++ sewer/tests/test_ACMEclient.py
@@ -146,6 +146,21 @@ class TestACMEclient(TestCase):
             self.client.cert()
             self.assertTrue(mock_create_dns_record.called)
 
+    def test_notify_acme_challenge_set_is_called(self):
+        with mock.patch('requests.post') as mock_requests_post, mock.patch(
+                'requests.get') as mock_requests_get, mock.patch(
+                    'sewer.Client.notify_acme_challenge_set'
+                ) as mock_notify_acme_challenge_set:
+            content = """
+                          {"challenges": [{"type": "dns-01", "token": "example-token", "uri": "example-uri"}]}
+                      """
+            mock_requests_post.return_value = test_utils.MockResponse(
+                content=content)
+            mock_requests_get.return_value = test_utils.MockResponse(
+                content=content)
+            self.client.cert()
+            self.assertTrue(mock_notify_acme_challenge_set.called)
+
 
 # TEST cli
 # from unittest import TestCase
