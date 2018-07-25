ca631b73c7a6be5c837adbc34b36348b9adfada9
menduo <shimenduo@gmail.com>
2018-07-25 11:31:00 +0800

    setup: update extra require, aliyunsdkcore to aliyunsdkcore-v3

--- setup.py
+++ setup.py
@@ -84,8 +84,8 @@ setup(
     extras_require={
         'dev': ['coverage', 'pypandoc', 'twine', 'wheel'],
         'test': ['flake8==3.5.0', 'mock', 'pylint==1.8.2', 'pycodestyle==2.3.1'],
-        'aliyun': ['aliyunsdkcore-v3', 'aliyun-python-sdk-alidns'],
-        'alicloud': ['aliyunsdkcore-v3', 'aliyun-python-sdk-alidns'],
+        'aliyun': ['aliyun-python-sdk-core-v3', 'aliyun-python-sdk-alidns'],
+        'alicloud': ['aliyun-python-sdk-core-v3', 'aliyun-python-sdk-alidns'],
     },
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
