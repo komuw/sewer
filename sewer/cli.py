d7a46607d113fe930d5bcb927bebca21170adb2a
menduo <shimenduo@gmail.com>
2018-07-25 20:06:43 +0800

    cli: enable people to specify the location where they want certificate and keys to be stored

--- sewer/cli.py
+++ sewer/cli.py
@@ -27,8 +27,6 @@ def main():
         --domain example.com \
         --action renew
     """
-    # TODO: enable people to specify the location where they want certificate and keys to be stored.
-    # currently, we store them in the directory from which sewer is ran
     parser = argparse.ArgumentParser(prog='sewer',
                                      description="""Sewer is a Let's Encrypt(ACME) client.
             Example usage::
@@ -108,6 +106,16 @@ def main():
         Either run (get a new certificate) or renew (renew a certificate). \
         eg: --action run")
     parser.add_argument(
+        "--out_dir",
+        type=str,
+        required=False,
+        default=os.getcwd(),
+        help="""The dir where the certificate and keys file will be stored.
+            default:  The directory you run sewer command.
+            eg: --out_dir /data/ssl/
+            """
+    )
+    parser.add_argument(
         "--loglevel",
         type=str,
         required=False,
@@ -128,6 +136,10 @@ def main():
     endpoint = args.endpoint
     email = args.email
     loglevel = args.loglevel
+    out_dir = args.out_dir
+
+    if not os.access(out_dir, os.W_OK):
+        raise OSError("The dir '{0}' is not writeable".format(out_dir))
 
     logger = logging.getLogger()
     handler = logging.StreamHandler()
@@ -236,10 +248,15 @@ def main():
     certificate_key = client.certificate_key
     account_key = client.account_key
 
-    # write out account_key in current directory
-    with open('{0}.account.key'.format(file_name), 'w') as account_file:
+    # prepare file path
+    account_key_file_path = os.path.join(out_dir, '{0}.account.key'.format(file_name))
+    crt_file_path = os.path.join(out_dir, '{0}.crt'.format(file_name))
+    crt_key_file_path = os.path.join(out_dir, '{0}.key'.format(file_name))
+
+    # write out account_key in out_dir directory
+    with open(account_key_file_path, 'w') as account_file:
         account_file.write(account_key)
-    logger.info('account key succesfully written to current directory.')
+    logger.info('account key succesfully written to {0}.'.format(account_key_file_path))
 
     if action == 'renew':
         message = 'Certificate Succesfully renewed. The certificate, certificate key and account key have been saved in the current directory'
@@ -248,10 +265,13 @@ def main():
         message = 'Certificate Succesfully issued. The certificate, certificate key and account key have been saved in the current directory'
         certificate = client.cert()
 
-    # write out certificate and certificate key in current directory
-    with open('{0}.crt'.format(file_name), 'w') as certificate_file:
+    # write out certificate and certificate key in out_dir directory
+    with open(crt_file_path, 'w') as certificate_file:
         certificate_file.write(certificate)
-    with open('{0}.key'.format(file_name), 'w') as certificate_key_file:
+    with open(crt_key_file_path, 'w') as certificate_key_file:
         certificate_key_file.write(certificate_key)
+
+    logger.info('certificate succesfully written to {0}.'.format(crt_file_path))
+    logger.info('certificate key succesfully written to {0}.'.format(crt_key_file_path))
 
     logger.info("the_end. {0}".format(message))
