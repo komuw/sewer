e65401d3d5626829df447ddfc7ea001f0987ea56
Tungsteno74 <tungsteno74@yahoo.it>
2019-06-05 21:49:01 +0200

    Create duckdns.py

---
+++ sewer/dns_providers/duckdns.py
@@ -0,0 +1,11 @@
+import sewer
+
+class DuckDNSDns(sewer.BaseDns):
+    def __init__(sellf):
+        pass
+       
+    def create_dns_record(self):
+        pass
+        
+    def delete_dns_record(self):
+        pass
