26cf0d8292b899c4514de7dba33358fd834db681
Tungsteno74 <tungsteno74@yahoo.it>
2019-06-12 19:43:57 +0200

    Update duckdns.py

--- sewer/dns_providers/duckdns.py
+++ sewer/dns_providers/duckdns.py
@@ -39,5 +39,17 @@ class DuckDNSDns(common.BaseDns):
 
     def delete_dns_record(self, domain_name, domain_dns_value):
         self.logger.info("delete_dns_record")
-        # acme-dns doesn't support this
+        domain_name = domain_name.lstrip("*.")
+
+        url = urllib.parse.urljoin(self.DUCKDNS_API_BASE_URL, "update")
+
+        payload = { "domains": domain_name, "token": self.duckdns_token , "txt": "removed"}
+        update_duckdns_dns_record_response = requests.get(url, params=payload, timeout=self.HTTP_TIMEOUT)
+        self.logger.debug("update_duckdns_dns_record_response. status_code={0}. response={1}".format(update_duckdns_dns_record_response.status_code,
+                self.log_response(update_duckdns_dns_record_response),))
+        if update_duckdns_dns_record_response.status_code != 200:
+            # raise error so that we do not continue to make calls to DuckDNS
+            # server
+            raise ValueError("Error removing DuckDNS dns record: status_code={status_code} response={response}".format(status_code=update_acmedns_dns_record_response.status_code,
+                    response=self.log_response(update_duckdns_dns_record_response),))
         self.logger.info("delete_dns_record_success")
