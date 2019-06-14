28caa8e9f31524ed841483adfc0fcb189507ff46
Tungsteno74 <tungsteno74@yahoo.it>
2019-06-14 04:00:36 +0200

    Update duckdns.py

--- sewer/dns_providers/duckdns.py
+++ sewer/dns_providers/duckdns.py
@@ -16,7 +16,7 @@ class DuckDNSDns(common.BaseDns):
             self.DUCKDNS_API_BASE_URL = DUCKDNS_API_BASE_URL + "/"
         else:
             self.DUCKDNS_API_BASE_URL = DUCKDNS_API_BASE_URL
-        super(DuckDNSDns, self).__init__("DEBUG")     
+        super(DuckDNSDns, self).__init__()     
 
     def create_dns_record(self, domain_name, domain_dns_value):
         self.logger.info("create_dns_record")
@@ -28,7 +28,7 @@ class DuckDNSDns(common.BaseDns):
 
         payload = { "domains": domain_name, "token": self.duckdns_token , "txt": domain_dns_value}
         update_duckdns_dns_record_response = requests.get(url, params=payload, timeout=self.HTTP_TIMEOUT)
-        self.logger.debug("update_duckdns_dns_record_response. status_code={0}. response={1}".format(update_duckdns_dns_record_response.status_code,
+        self.logger.info("update_duckdns_dns_record_response. status_code={0}. response={1}".format(update_duckdns_dns_record_response.status_code,
                 self.log_response(update_duckdns_dns_record_response),))
         if update_duckdns_dns_record_response.status_code != 200:
             # raise error so that we do not continue to make calls to DuckDNS
