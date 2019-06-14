ea6fd4d3e05b6adb6ea5d5ae36c9e90d7830431c
Tungsteno74 <tungsteno74@yahoo.it>
2019-06-14 17:19:29 +0200

    Update duckdns.py

--- sewer/dns_providers/duckdns.py
+++ sewer/dns_providers/duckdns.py
@@ -16,17 +16,15 @@ class DuckDNSDns(common.BaseDns):
             self.DUCKDNS_API_BASE_URL = DUCKDNS_API_BASE_URL + "/"
         else:
             self.DUCKDNS_API_BASE_URL = DUCKDNS_API_BASE_URL
-        super(DuckDNSDns, self).__init__()     
-
-    def create_dns_record(self, domain_name, domain_dns_value):
-        self.logger.info("create_dns_record")
-
+        super(DuckDNSDns, self).__init__()   
+        
+    def _common_dns_record(self, domain_name, req_last):
         # if we have been given a wildcard name, strip wildcard
         domain_name = domain_name.lstrip("*.")
-
+        
         url = urllib.parse.urljoin(self.DUCKDNS_API_BASE_URL, "update")
 
-        payload = { "domains": domain_name, "token": self.duckdns_token , "txt": domain_dns_value}
+        payload = dict(("domains", domain_name), ("token", self.duckdns_token), ("txt", domain_dns_value), req_last)
         update_duckdns_dns_record_response = requests.get(url, params=payload, timeout=self.HTTP_TIMEOUT)
         
         normalized_response = self.log_response(update_duckdns_dns_record_response).decode("utf8")
@@ -38,26 +36,13 @@ class DuckDNSDns(common.BaseDns):
             # server
             raise ValueError("Error creating DuckDNS dns record: status_code={status_code} response={response}".format(
                     status_code=update_duckdns_dns_record_response.status_code, response=normalized_response,))
-
+            
+    def create_dns_record(self, domain_name, domain_dns_value):
+        self.logger.info("create_dns_record")       
+        self._common_dns_record(self, domain_name, ("txt": domain_dns_value))
         self.logger.info("create_dns_record_success")   
 
     def delete_dns_record(self, domain_name, domain_dns_value):
         self.logger.info("delete_dns_record")
-        domain_name = domain_name.lstrip("*.")
-
-        url = urllib.parse.urljoin(self.DUCKDNS_API_BASE_URL, "update")
-
-        payload = { "domains": domain_name, "token": self.duckdns_token , "clear": "true"}
-        update_duckdns_dns_record_response = requests.get(url, params=payload, timeout=self.HTTP_TIMEOUT)
-        
-        normalized_response = self.log_response(update_duckdns_dns_record_response).decode("utf8")
-        self.logger.debug("update_duckdns_dns_record_response. status_code={0}. response={1}".format(
-                update_duckdns_dns_record_response.status_code, normalized_response,))
-        
-        if update_duckdns_dns_record_response.status_code != 200 or normalized_response != 'OK':
-            # raise error so that we do not continue to make calls to DuckDNS
-            # server
-            raise ValueError("Error removing DuckDNS dns record: status_code={status_code} response={response}".format(
-                    status_code=update_acmedns_dns_record_response.status_code, response=normalized_response,))
-
+        self._common_dns_record(self, domain_name, ("clear": "true"))
         self.logger.info("delete_dns_record_success")
