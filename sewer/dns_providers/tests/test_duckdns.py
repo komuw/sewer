db820662be84d45aa45785af6cb445278102e676
Tungsteno74 <tungsteno74@yahoo.it>
2019-06-18 18:42:50 +0200

    Update test_duckdns.py

--- sewer/dns_providers/tests/test_duckdns.py
+++ sewer/dns_providers/tests/test_duckdns.py
@@ -17,8 +17,10 @@ class TestDuckDNS(TestCase):
         self.duckdns_API_KEY = "mock-api-key"
         self.duckdns_API_BASE_URL = "https://some-mock-url.com"
 
+        self.duck_mresponse = test_utils.MockResponse()
+        self.duck_mresponse.content = "OK"
         with mock.patch("requests.get") as mock_requests_get:
-            mock_requests_get.return_value = test_utils.MockResponse()
+            mock_requests_get.return_value = self.duck_mresponse
             self.dns_class = sewer.DuckDNSDns(
                 duckdns_token=self.duckdns_API_KEY, DUCKDNS_API_BASE_URL=self.duckdns_API_BASE_URL
             )
@@ -30,9 +32,10 @@ class TestDuckDNS(TestCase):
         with mock.patch("requests.get") as mock_requests_get, mock.patch(
             "sewer.DuckDNSDns.delete_dns_record"
         ) as mock_delete_dns_record:
+
             mock_requests_get.return_value = (
                 mock_delete_dns_record.return_value
-            ) = test_utils.MockResponse(content="OK")
+            ) = self.duck_mresponse
             self.dns_class.create_dns_record(
                 domain_name=self.domain_name, domain_dns_value=self.domain_dns_value
             )
@@ -47,7 +50,7 @@ class TestDuckDNS(TestCase):
 
     def test_duckdns_is_not_called_by_delete_dns_record(self):
         with mock.patch("requests.get") as mock_requests_get:
-            mock_requests_get.return_value = test_utils.MockResponse(content="OK")
+            mock_requests_get.return_value = self.duck_mresponse
             self.dns_class.delete_dns_record(
                 domain_name=self.domain_name, domain_dns_value=self.domain_dns_value
             )
@@ -57,7 +60,7 @@ class TestDuckDNS(TestCase):
         with mock.patch("requests.get") as mock_requests_get, mock.patch(
             "requests.delete"
         ) as mock_requests_delete:
-            mock_requests_get.return_value = test_utils.MockResponse(content="OK")
+            mock_requests_get.return_value = self.duck_mresponse
             self.dns_class.delete_dns_record(
                 domain_name=self.domain_name, domain_dns_value=self.domain_dns_value
             )
