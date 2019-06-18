a943f56a770fdc4872ebf9c0ca25b69f46d46711
Tungsteno74 <tungsteno74@yahoo.it>
2019-06-18 20:18:02 +0200

    Update duckdns.py

--- sewer/dns_providers/duckdns.py
+++ sewer/dns_providers/duckdns.py
@@ -31,7 +31,7 @@ class DuckDNSDns(common.BaseDns):
             url, params=payload, timeout=self.HTTP_TIMEOUT
         )
 
-        normalized_response = update_duckdns_dns_record_response.content.decode("utf8")
+        normalized_response = update_duckdns_dns_record_response.text
         self.logger.debug(
             "update_duckdns_dns_record_response. status_code={0}. response={1}".format(
                 update_duckdns_dns_record_response.status_code, normalized_response
