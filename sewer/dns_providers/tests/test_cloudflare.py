eb4fcfd8e89b6fe3a852a1488ef92533cfac490d
Moritz Ulmer <moritz.ulmer@posteo.de>
2019-12-05 11:40:21 +0100

    Add support for token authentication in Cloudflare
    Why:
    
    - Tokens allow limiting access to the Cloudflare API
    
    This change addresses the need by:
    
    - Extending the Cloudflare constructor signature
    - Adding validation of authentication options
    - Adding tests to cover the new behaviour
    - Updating the CLI

--- sewer/dns_providers/tests/test_cloudflare.py
+++ sewer/dns_providers/tests/test_cloudflare.py
@@ -17,15 +17,26 @@ class TestCloudflare(TestCase):
         self.CLOUDFLARE_EMAIL = "mock-email@example.com"
         self.CLOUDFLARE_API_KEY = "mock-api-key"
         self.CLOUDFLARE_API_BASE_URL = "https://some-mock-url.com"
+        self.CLOUDFLARE_TOKEN = "mock-token"
 
         with mock.patch("requests.post") as mock_requests_post, mock.patch(
             "requests.get"
         ) as mock_requests_get:
             mock_requests_post.return_value = test_utils.MockResponse()
             mock_requests_get.return_value = test_utils.MockResponse()
-            self.dns_class = sewer.CloudFlareDns(
+            self.dns_class_api_key = sewer.CloudFlareDns(
                 CLOUDFLARE_EMAIL=self.CLOUDFLARE_EMAIL,
                 CLOUDFLARE_API_KEY=self.CLOUDFLARE_API_KEY,
+                CLOUDFLARE_API_BASE_URL=self.CLOUDFLARE_API_BASE_URL,
+            )
+
+        with mock.patch("requests.post") as mock_requests_post, mock.patch(
+            "requests.get"
+        ) as mock_requests_get:
+            mock_requests_post.return_value = test_utils.MockResponse()
+            mock_requests_get.return_value = test_utils.MockResponse()
+            self.dns_class_token = sewer.CloudFlareDns(
+                CLOUDFLARE_TOKEN=self.CLOUDFLARE_TOKEN,
                 CLOUDFLARE_API_BASE_URL=self.CLOUDFLARE_API_BASE_URL,
             )
 
@@ -44,7 +55,12 @@ class TestCloudflare(TestCase):
                 mock_requests_delete.return_value
             ) = mock_delete_dns_record.return_value = test_utils.MockResponse()
 
-            self.dns_class.create_dns_record(
+            self.dns_class_api_key.create_dns_record(
+                domain_name=self.domain_name, domain_dns_value=self.domain_dns_value
+            )
+            self.assertFalse(mock_delete_dns_record.called)
+
+            self.dns_class_token.create_dns_record(
                 domain_name=self.domain_name, domain_dns_value=self.domain_dns_value
             )
             self.assertFalse(mock_delete_dns_record.called)
@@ -61,7 +77,8 @@ class TestCloudflare(TestCase):
                 mock_requests_delete.return_value
             ) = mock_delete_dns_record.return_value = test_utils.MockResponse()
 
-            self.dns_class.create_dns_record(
+            # Test with API key authentication
+            self.dns_class_api_key.create_dns_record(
                 domain_name=self.domain_name, domain_dns_value=self.domain_dns_value
             )
             expected = {
@@ -69,6 +86,21 @@ class TestCloudflare(TestCase):
                     "X-Auth-Email": self.CLOUDFLARE_EMAIL,
                     "X-Auth-Key": self.CLOUDFLARE_API_KEY,
                 },
+                "data": '{"content": "mock-domain_dns_value", "type": "TXT", "name": "_acme-challenge.example.com."}',
+                "timeout": 65,
+            }
+
+            self.assertDictEqual(expected["headers"], mock_requests_post.call_args[1]["headers"])
+            self.assertDictEqual(
+                json.loads(expected["data"]), mock_requests_post.call_args[1]["json"]
+            )
+
+            # Test with token authentication
+            self.dns_class_token.create_dns_record(
+                domain_name=self.domain_name, domain_dns_value=self.domain_dns_value
+            )
+            expected = {
+                "headers": {"Authorization": "Bearer " + self.CLOUDFLARE_TOKEN},
                 "data": '{"content": "mock-domain_dns_value", "type": "TXT", "name": "_acme-challenge.example.com."}',
                 "timeout": 65,
             }
@@ -88,7 +120,8 @@ class TestCloudflare(TestCase):
 
             mock_requests_get.return_value = test_utils.MockResponse()
 
-            self.dns_class.delete_dns_record(
+            # Test with API key authentication
+            self.dns_class_api_key.delete_dns_record(
                 domain_name=self.domain_name, domain_dns_value=self.domain_dns_value
             )
             self.assertTrue(mock_requests_get.called)
@@ -104,3 +137,57 @@ class TestCloudflare(TestCase):
                 "https://some-mock-url.com/zones/None/dns_records/some-mock-dns-zone-id",
                 str(mock_requests_delete.call_args),
             )
+
+            # Test with token authentication
+            self.dns_class_token.delete_dns_record(
+                domain_name=self.domain_name, domain_dns_value=self.domain_dns_value
+            )
+            self.assertTrue(mock_requests_get.called)
+            expected = {
+                "headers": {"Authorization": "Bearer " + self.CLOUDFLARE_TOKEN},
+                "timeout": 65,
+            }
+            self.assertDictEqual(expected, mock_requests_delete.call_args[1])
+            self.assertIn(
+                "https://some-mock-url.com/zones/None/dns_records/some-mock-dns-zone-id",
+                str(mock_requests_delete.call_args),
+            )
+
+
+class TestCloudflareTokens(TestCase):
+    """
+    Test that the authorization parameters are validated (either email + API key or token)
+    """
+
+    def setUp(self):
+        self.CLOUDFLARE_EMAIL = "mock-email@example.com"
+        self.CLOUDFLARE_API_KEY = "mock-api-key"
+        self.CLOUDFLARE_TOKEN = "some-token"
+
+    def test_init_auth_validation(self):
+        # Invalid inputs
+        with self.assertRaises(ValueError):
+            sewer.CloudFlareDns(
+                CLOUDFLARE_TOKEN=self.CLOUDFLARE_TOKEN,
+                CLOUDFLARE_EMAIL=self.CLOUDFLARE_EMAIL,
+                CLOUDFLARE_API_KEY=self.CLOUDFLARE_API_KEY,
+            )
+
+        with self.assertRaises(ValueError):
+            sewer.CloudFlareDns(
+                CLOUDFLARE_TOKEN=self.CLOUDFLARE_TOKEN, CLOUDFLARE_API_KEY=self.CLOUDFLARE_API_KEY
+            )
+
+        with self.assertRaises(ValueError):
+            sewer.CloudFlareDns()
+
+        with self.assertRaises(ValueError):
+            sewer.CloudFlareDns(
+                CLOUDFLARE_TOKEN=self.CLOUDFLARE_TOKEN, CLOUDFLARE_EMAIL=self.CLOUDFLARE_EMAIL
+            )
+
+        # Valid inputs
+        sewer.CloudFlareDns(
+            CLOUDFLARE_EMAIL=self.CLOUDFLARE_EMAIL, CLOUDFLARE_API_KEY=self.CLOUDFLARE_API_KEY
+        )
+        sewer.CloudFlareDns(CLOUDFLARE_TOKEN=self.CLOUDFLARE_TOKEN)
