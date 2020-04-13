3355b3cf873c7c15e6b1615626ffe7469ff8690c
Alan Bacon <alan.bacon@bbc.co.uk>
2020-04-13 18:07:04 +0100

    format code using black

--- sewer/dns_providers/gandi.py
+++ sewer/dns_providers/gandi.py
@@ -15,7 +15,7 @@ class GandiDns(common.BaseDns):
         GANDI_API_KEY=None,
         GANDI_API_BASE_URL="https://dns.api.gandi.net/api/v5/",
         DEFAULT_TTL=10800,
-        requests_lib=requests
+        requests_lib=requests,
     ):
         self.GANDI_API_KEY = GANDI_API_KEY
         self.HTTP_TIMEOUT = 65  # seconds
@@ -29,12 +29,13 @@ class GandiDns(common.BaseDns):
 
         # pass an API key
         if not GANDI_API_KEY:
-            raise ValueError(
-                "Error initializing Gandi DNS adapter. Pass an API key."
-            )
+            raise ValueError("Error initializing Gandi DNS adapter. Pass an API key.")
 
         self.GET_HEADERS = {"X-Api-Key": self.GANDI_API_KEY}
-        self.POST_HEADERS = {"X-Api-Key": self.GANDI_API_KEY, "Content-Type": "application/json"}
+        self.POST_HEADERS = {
+            "X-Api-Key": self.GANDI_API_KEY,
+            "Content-Type": "application/json",
+        }
 
         super(GandiDns, self).__init__()
 
@@ -44,31 +45,33 @@ class GandiDns(common.BaseDns):
         zone_records_href = self.get_zone_records_href(base_domain)
 
         request_body = {
-            "rrset_type": 'TXT',
+            "rrset_type": "TXT",
             "rrset_ttl": self.RECORD_TTL,
             "rrset_name": GandiDns.subdomain_to_challenge_domain(subdomain),
-            "rrset_values": [domain_dns_value]
+            "rrset_values": [domain_dns_value],
         }
 
         create_record_resp = self.requests.post(
-            zone_records_href,
-            headers=self.POST_HEADERS,
-            json=request_body
+            zone_records_href, headers=self.POST_HEADERS, json=request_body
         )
         if not create_record_resp.status_code < 300:
-            raise RuntimeError('createRecord failed')
+            raise RuntimeError("createRecord failed")
 
     def delete_dns_record(self, domain_name, domain_dns_value):
         self.delete_record(domain_name)
 
     def delete_record(self, domain_name, idempotent=False):
         def get_subdomain_record(subdomain, all_records):
-            subdomain_records = list(filter(lambda rec: rec['rrset_name'] == subdomain, all_records))
+            subdomain_records = list(
+                filter(lambda rec: rec["rrset_name"] == subdomain, all_records)
+            )
             if not len(subdomain_records) == 1:
                 if idempotent:
                     return None
                 else:
-                    raise RuntimeError('there is not exactly one record for domain: ' + domain_name)
+                    raise RuntimeError(
+                        "there is not exactly one record for domain: " + domain_name
+                    )
 
             return subdomain_records[0]
 
@@ -76,37 +79,34 @@ class GandiDns(common.BaseDns):
         zone_records_href = self.get_zone_records_href(base_domain)
         all_records = self.get_all_zone_records(zone_records_href)
         subdomain_record = get_subdomain_record(
-            GandiDns.subdomain_to_challenge_domain(subdomain),
-            all_records
+            GandiDns.subdomain_to_challenge_domain(subdomain), all_records
         )
 
         if subdomain_record:
             del_record_resp = self.requests.delete(
-                subdomain_record['rrset_href'],
-                headers=self.GET_HEADERS
+                subdomain_record["rrset_href"], headers=self.GET_HEADERS
             )
             if not del_record_resp.status_code < 300:
-                raise RuntimeError('deleteRecord failed')
+                raise RuntimeError("deleteRecord failed")
 
     def get_zone_records_href(self, base_domain):
         domain_resp = self.requests.get(
-            os.path.join(self.GANDI_API_BASE_URL, 'domains', base_domain),
-            headers=self.GET_HEADERS
+            os.path.join(self.GANDI_API_BASE_URL, "domains", base_domain),
+            headers=self.GET_HEADERS,
         )
 
         if not domain_resp.status_code < 300:
-            raise RuntimeError('getZoneHref failed')
+            raise RuntimeError("getZoneHref failed")
 
         domain_info = domain_resp.json()
-        return domain_info['zone_records_href']
+        return domain_info["zone_records_href"]
 
     def get_all_zone_records(self, zone_records_href):
         all_records_resp = self.requests.get(
-            zone_records_href,
-            headers=self.GET_HEADERS
+            zone_records_href, headers=self.GET_HEADERS
         )
         if not all_records_resp.status_code < 300:
-            raise RuntimeError('getAllZoneRecords failed')
+            raise RuntimeError("getAllZoneRecords failed")
 
         all_records = all_records_resp.json()
         return all_records
@@ -116,15 +116,17 @@ class GandiDns(common.BaseDns):
         def has_second_level_domain(split_domains):
             return len(split_domains) >= 2
 
-        split_domains = domain_name.split('.')
+        split_domains = domain_name.split(".")
         if not has_second_level_domain(split_domains):
-            raise RuntimeError('domain: ' + str(domain_name) + 'does not have a second level domain')
+            raise RuntimeError(
+                "domain: " + str(domain_name) + "does not have a second level domain"
+            )
 
-        separator = '.'
+        separator = "."
         base_domain = separator.join(split_domains[-2:])
         subdomain = separator.join(split_domains[0:-2])
         if not subdomain:
-            subdomain = '@'
+            subdomain = "@"
 
         return [subdomain, base_domain]
 
