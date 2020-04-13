b72fbb9da4f68a0c56cd404bc5d73352e3b29571
Alan Bacon <alan.bacon@bbc.co.uk>
2020-04-13 17:52:02 +0100

    remove standalone assert statement

--- sewer/dns_providers/tests/test_gandi.py
+++ sewer/dns_providers/tests/test_gandi.py
@@ -16,8 +16,6 @@ class MockResponseObject:
 
 def add_side_effect_to_request_get(mock_requests_get):
     def mock_implementation(*args, **kwargs):
-        assert kwargs['headers'] == {"X-Api-Key": MOCK_GANDI_API_KEY}, 'Get headers not set'
-
         if args[0] == 'https://dns.api.gandi.net/api/v5/domains/second-level-domain.tld':
             return MockResponseObject(body={
                 'zone_records_href': 'mock_zone_records_href'
@@ -43,11 +41,6 @@ def add_side_effect_to_request_get(mock_requests_get):
 
 def add_side_effect_to_request_post(mock_requests_post):
     def mock_implementation(*args, **kwargs):
-        assert kwargs['headers'] == {
-            "X-Api-Key": MOCK_GANDI_API_KEY,
-            "Content-Type": "application/json"},\
-            'Post headers not set'
-
         if args[0] == 'mock_zone_records_href':
             return MockResponseObject()
 
@@ -58,8 +51,6 @@ def add_side_effect_to_request_post(mock_requests_post):
 
 def add_side_effect_to_request_delete(mock_requests_delete):
     def mock_implementation(*args, **kwargs):
-        assert kwargs['headers'] == {"X-Api-Key": MOCK_GANDI_API_KEY}, 'Get headers not set'
-
         if args[0] == 'mock_record_href':
             return MockResponseObject()
 
@@ -93,17 +84,33 @@ def mock_requests(func, mock_requests_get, mock_requests_post, mock_requests_del
 
 class TestGandiDns(TestCase):
 
+    EXPECTED_GET_HEADERS = {
+        "X-Api-Key": MOCK_GANDI_API_KEY
+    }
+
+    EXPECTED_POST_HEADERS = {
+        "X-Api-Key": MOCK_GANDI_API_KEY,
+        "Content-Type": "application/json"
+    }
+
+    def check_correct_headers_passed(self, calls, expectedHeaders):
+        for call in calls:
+            self.assertEqual(call[1]['headers'], expectedHeaders)
+
     @mock_requests
     def test_delete_existing_record(self, mock_requests_lib):
         gandi_dns = sewer.GandiDns(GANDI_API_KEY=MOCK_GANDI_API_KEY, requests_lib=mock_requests_lib)
         gandi_dns.delete_dns_record('subsubdomain.subdomain.second-level-domain.tld', 'val')
 
-        getCalls = mock_requests_lib.get.call_args_list
-        deleteCalls = mock_requests_lib.delete.call_args_list
+        get_calls = mock_requests_lib.get.call_args_list
+        delete_calls = mock_requests_lib.delete.call_args_list
 
-        self.assertEqual(getCalls[0][0][0], 'https://dns.api.gandi.net/api/v5/domains/second-level-domain.tld')
-        self.assertEqual(getCalls[1][0][0], 'mock_zone_records_href')
-        self.assertEqual(deleteCalls[0][0][0], 'mock_record_href')
+        self.check_correct_headers_passed(get_calls, TestGandiDns.EXPECTED_GET_HEADERS)
+        self.check_correct_headers_passed(delete_calls, TestGandiDns.EXPECTED_GET_HEADERS)
+
+        self.assertEqual(get_calls[0][0][0], 'https://dns.api.gandi.net/api/v5/domains/second-level-domain.tld')
+        self.assertEqual(get_calls[1][0][0], 'mock_zone_records_href')
+        self.assertEqual(delete_calls[0][0][0], 'mock_record_href')
 
     @mock_requests
     def test_delete_non_existing_record(self, mock_requests_lib):
@@ -118,37 +125,45 @@ class TestGandiDns(TestCase):
         gandi_dns = sewer.GandiDns(GANDI_API_KEY=MOCK_GANDI_API_KEY, requests_lib=mock_requests_lib)
         gandi_dns.create_dns_record('subsubdomain.subdomain.second-level-domain.tld', 'val')
 
-        getCalls = mock_requests_lib.get.call_args_list
-        postCalls = mock_requests_lib.post.call_args_list
-        deleteCalls = mock_requests_lib.delete.call_args_list
+        get_calls = mock_requests_lib.get.call_args_list
+        post_calls = mock_requests_lib.post.call_args_list
+        delete_calls = mock_requests_lib.delete.call_args_list
 
-        self.assertEqual(getCalls[0][0][0], 'https://dns.api.gandi.net/api/v5/domains/second-level-domain.tld')
-        self.assertEqual(getCalls[1][0][0], 'mock_zone_records_href')
-        self.assertEqual(deleteCalls[0][0][0], 'mock_record_href')
+        self.check_correct_headers_passed(get_calls, TestGandiDns.EXPECTED_GET_HEADERS)
+        self.check_correct_headers_passed(post_calls, TestGandiDns.EXPECTED_POST_HEADERS)
+        self.check_correct_headers_passed(delete_calls, TestGandiDns.EXPECTED_GET_HEADERS)
 
-        self.assertEqual(getCalls[2][0][0], 'https://dns.api.gandi.net/api/v5/domains/second-level-domain.tld')
-        self.assertEqual(postCalls[0][0][0], 'mock_zone_records_href')
-        self.assertEqual(postCalls[0][1]['json']['rrset_type'], 'TXT')
-        self.assertEqual(postCalls[0][1]['json']['rrset_ttl'], 10800)
-        self.assertEqual(postCalls[0][1]['json']['rrset_name'], '_acme-challenge.subsubdomain.subdomain')
-        self.assertEqual(postCalls[0][1]['json']['rrset_values'], ['val'])
+        self.assertEqual(get_calls[0][0][0], 'https://dns.api.gandi.net/api/v5/domains/second-level-domain.tld')
+        self.assertEqual(get_calls[1][0][0], 'mock_zone_records_href')
+        self.assertEqual(delete_calls[0][0][0], 'mock_record_href')
+
+        self.assertEqual(get_calls[2][0][0], 'https://dns.api.gandi.net/api/v5/domains/second-level-domain.tld')
+        self.assertEqual(post_calls[0][0][0], 'mock_zone_records_href')
+        self.assertEqual(post_calls[0][1]['json']['rrset_type'], 'TXT')
+        self.assertEqual(post_calls[0][1]['json']['rrset_ttl'], 10800)
+        self.assertEqual(post_calls[0][1]['json']['rrset_name'], '_acme-challenge.subsubdomain.subdomain')
+        self.assertEqual(post_calls[0][1]['json']['rrset_values'], ['val'])
 
     @mock_requests
     def test_create_non_existing_record(self, mock_requests_lib):
         gandi_dns = sewer.GandiDns(GANDI_API_KEY=MOCK_GANDI_API_KEY, requests_lib=mock_requests_lib)
         gandi_dns.create_dns_record('subdomain.second-level-domain.tld', 'val')
 
-        getCalls = mock_requests_lib.get.call_args_list
-        postCalls = mock_requests_lib.post.call_args_list
-        deleteCalls = mock_requests_lib.delete.call_args_list
+        get_calls = mock_requests_lib.get.call_args_list
+        post_calls = mock_requests_lib.post.call_args_list
+        delete_calls = mock_requests_lib.delete.call_args_list
 
-        self.assertEqual(getCalls[0][0][0], 'https://dns.api.gandi.net/api/v5/domains/second-level-domain.tld')
-        self.assertEqual(getCalls[1][0][0], 'mock_zone_records_href')
-        self.assertEqual(len(deleteCalls), 0)
+        self.check_correct_headers_passed(get_calls, TestGandiDns.EXPECTED_GET_HEADERS)
+        self.check_correct_headers_passed(post_calls, TestGandiDns.EXPECTED_POST_HEADERS)
+        self.check_correct_headers_passed(delete_calls, TestGandiDns.EXPECTED_GET_HEADERS)
 
-        self.assertEqual(getCalls[2][0][0], 'https://dns.api.gandi.net/api/v5/domains/second-level-domain.tld')
-        self.assertEqual(postCalls[0][0][0], 'mock_zone_records_href')
-        self.assertEqual(postCalls[0][1]['json']['rrset_type'], 'TXT')
-        self.assertEqual(postCalls[0][1]['json']['rrset_ttl'], 10800)
-        self.assertEqual(postCalls[0][1]['json']['rrset_name'], '_acme-challenge.subdomain')
-        self.assertEqual(postCalls[0][1]['json']['rrset_values'], ['val'])
+        self.assertEqual(get_calls[0][0][0], 'https://dns.api.gandi.net/api/v5/domains/second-level-domain.tld')
+        self.assertEqual(get_calls[1][0][0], 'mock_zone_records_href')
+        self.assertEqual(len(delete_calls), 0)
+
+        self.assertEqual(get_calls[2][0][0], 'https://dns.api.gandi.net/api/v5/domains/second-level-domain.tld')
+        self.assertEqual(post_calls[0][0][0], 'mock_zone_records_href')
+        self.assertEqual(post_calls[0][1]['json']['rrset_type'], 'TXT')
+        self.assertEqual(post_calls[0][1]['json']['rrset_ttl'], 10800)
+        self.assertEqual(post_calls[0][1]['json']['rrset_name'], '_acme-challenge.subdomain')
+        self.assertEqual(post_calls[0][1]['json']['rrset_values'], ['val'])
