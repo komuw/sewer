d5d24554c2df65a9c3103e9feae2dd4f9ab2dc0f
Martin Maney <maney@two14.net>
2024-11-12 22:53:25 -0600

    CI/etc. try running CI on 3.13 - may require further updates

--- setup.py
+++ setup.py
@@ -40,11 +40,11 @@ setup(
         "License :: OSI Approved :: MIT License",
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
     ],
     packages=find_packages(exclude=["docs", "*tests*"]),
     install_requires=["requests", "cryptography"],
